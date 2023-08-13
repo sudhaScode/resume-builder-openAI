70a42fb4546c486b58e0ddaaef9877b28be260a8
sudhaScode <sunriser7995@gmail.com>
2023-08-13 22:25:52 +0530

    large

---
+++ Javascript.js
@@ -0,0 +1,34 @@
+
+ let n = 5;
+ row =1;
+ for (let i=0;i <n;i++){
+    for(let j  = 0;j < n; j++ ){
+        if (row = i){
+            console.log( " ");
+             row++;
+        }
+        else
+        console.log("*");
+    }
+   console.log("new line");
+ }
+ /*class Javascript{
+  constructor (name, year, age){
+  this.year = year;
+  this.nane = name;
+  this.age = age; 
+  }
+  calculateage( age) {
+    let totalcalage = 0;
+   while(age <80){
+    totalcalage = age+totalcalage;
+   }
+   return totalcalage;   
+  }
+
+ }
+ const  date = new Date();
+ let year = date.getMonth;
+
+ const  mylearn = new Javascript("snbram", year, 24);
+  console.log(" My learnig is continuing at  " +  mylearn.nane +""+ mylearn.calculateag(age) + "Fuck the world create your own thoughts and entertainment");*/
